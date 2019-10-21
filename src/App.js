7802b648c98ff2aabc25a8f16a9c37088bf3182c
vant3x <alejandrovelasquezmolina@gmail.com>
2019-10-20 22:30:15 -0500

    Rrrrr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Frase from './components/Frase';
+import axios from 'axios';
 
 function App() {
+
+  const [frase, obtenerFrase] = useState({});
+
+  const fetchAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    obtenerFrase(resultado.data[0]);
+  }
+
+  useEffect( 
+    () => {
+      fetchAPI();
+    }, []
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Frase
+        frase={frase}
+      />
+      <button
+        onClick={fetchAPI}
+      >Generar Nueva Frase</button>
     </div>
   );
 }
